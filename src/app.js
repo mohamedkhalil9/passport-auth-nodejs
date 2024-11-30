271ca22b5c4a251384fff17d3c009a679ff3b2ea
mohamedkhalil9 <mohammedkhalil098@gmail.com>
2024-11-30 19:03:49 +0200

    db connection

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import appRouter from './api/routes/indexRouter.js';
+import connectDB from './config/db.js';
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
+app.use(express.json());
 app.use('/api/v1', appRouter);
 
 export default app;
---
+++ src/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const connecgDB = () => { 
+  mongoose
+    .connect(process.env.DB_URI)
+    .then((conn) => console.log(`db connected on ${conn.connection.host}`))
+    .catch((err) => console.log(err))
+} 
+
+
+export default connecgDB;
